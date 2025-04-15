1068d450f3677d95351a5a78564d42bbc1c52bcd
kimsearch <101050848+kimsearch@users.noreply.github.com>
2025-04-15 14:50:10 +0900

    fix:버그 해결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,6 @@ android {
 
 dependencies {
     // Retrofit + OkHttp + Gson
-    implementation("com.github.prolificinteractive:material-calendarview:2.0.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.3")
